11ea9735d49cc33e086bea4ca2d850782a8c6802
Kirill Korsuk <korsukkirill@gmail.com>
2023-07-25 18:46:35 +0300

    some commit

---
+++ src/test/java/Geekbrains/GameWindowTest.java
@@ -0,0 +1,20 @@
+package Geekbrains;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GameWindowTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
